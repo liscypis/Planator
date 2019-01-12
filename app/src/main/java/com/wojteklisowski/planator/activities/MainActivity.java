3f9412031ea8777a2ab0bf54a1f8d661f48dcfe4
Wojciech Lisowski <wojteklisowski95@gmail.com>
2019-01-12 22:01:55 +0100

    fixed SeekBars

--- app/src/main/java/com/wojteklisowski/planator/activities/MainActivity.java
+++ app/src/main/java/com/wojteklisowski/planator/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.wojteklisowski.planator.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -15,20 +16,23 @@ import android.widget.Toast;
 import com.google.android.gms.location.places.AutocompleteFilter;
 import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.Places;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.wojteklisowski.planator.R;
 import com.wojteklisowski.planator.adapters.PlaceAutocompleteArrayAdapter;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.time.Duration;
+import java.time.LocalTime;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
     private PlaceAutocompleteArrayAdapter madapter;
     private GeoDataClient mGeoDataClient;
 
-    private static final LatLngBounds BOUNDS = new LatLngBounds(new LatLng(49.053526, 13.858457), new LatLng(54.791003, 23.855164));
+    private int mDuration;
+    private int mDistance;
+    private boolean mManualMode = false;
 
+    //UI references.
     private AutoCompleteTextView mDestinationTv;
     private AutoCompleteTextView mOriginTv;
     private RadioGroup mTravelModeRg;
@@ -50,8 +54,120 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUIreferences();
 
+        mDurationSb.setMax(600 - 25); // minuty max 600 min 25
+        mDurationSb.setMin(0);
+        mLengthSb.setMax(500 - 20); //km  max 500 min 20
+        mLengthSb.setMin(0);
+        mDurationSb.setProgress(1);
 
+        // na poczatku sa ukryte
+        setInvisible();
+        mOKbnt.setEnabled(false);
+
+        mOKbnt.setOnClickListener(this);
+        mManualBnt.setOnClickListener(this);
+        mAutomaticBnt.setOnClickListener(this);
+        mTravelModeRg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rbCar:
+                        //srednia predkosc ok 50km/h = 0.8333333333333 km/min
+                        mDurationSb.setMax(600 - 25); // minuty max 600 min 25
+                        mDurationSb.setMin(0);
+                        mLengthSb.setMax(500 - 20); //km  max 500 min 20
+                        mDurationSb.setProgress(1);
+                        mLengthSb.setProgress(1);
+                        mDuration = 25;
+                        mDistance = 20;
+                        break;
+                    case R.id.rbBike:
+                        //srednia predkosc ok 17km/h = 0.2833333333333 km/min
+                        mDurationSb.setMax(600 - 53); // minuty max 600 min 53
+                        mDurationSb.setMin(0);
+                        mLengthSb.setMax(170 - 15); //km  max 170 min 15
+                        mDurationSb.setProgress(1);
+                        mLengthSb.setProgress(1);
+                        mDuration = 53;
+                        mDistance = 15;
+                        break;
+                    case R.id.rbWalk:
+                        //srednia predkosc ok 4,5km/h = 0.075 km/min
+                        mDurationSb.setMax(600 - 93); // minuty max 600 min 93
+                        mDurationSb.setMin(0);
+                        mLengthSb.setMax(45 - 7); //km  max 45 min 7
+                        mDurationSb.setProgress(1);
+                        mLengthSb.setProgress(1);
+                        mDuration = 93;
+                        mDistance = 7;
+                        break;
+                }
+            }
+        });
+
+        seekBarsChangeListener();
+
+        AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
+                .setCountry("PL")
+                .setTypeFilter(AutocompleteFilter.TYPE_FILTER_ADDRESS)
+                .build();
+
+        mGeoDataClient = Places.getGeoDataClient(this);
+        madapter = new PlaceAutocompleteArrayAdapter(this, mGeoDataClient, null, typeFilter);
+        mOriginTv.setAdapter(madapter);
+        mDestinationTv.setAdapter(madapter);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.bntOk:
+                if (!checkEditTexts()) {
+                    Toast.makeText(this, "Podaj punkt startowy i końcowy", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (checkPlaceType()) {
+                        intent = new Intent(this, MapsActivity.class);
+                        intent.putExtra("DESTINATION", mDestinationTv.getText().toString());
+                        intent.putExtra("ORIGIN", mOriginTv.getText().toString());
+                        intent.putExtra("TRAVEL_MODE", checkTravelMode());
+                        intent.putExtra("MANUAL_MODE", mManualMode);
+                        if (mManualMode) {
+                            intent.putExtra("DURATION", mDuration);
+                            intent.putExtra("DISTANCE", mDistance);
+                        }
+                        if (mMuseumCb.isChecked())
+                            intent.putExtra("TYPE", "museum");
+                        else
+                            intent.putExtra("TYPE", "park");
+                    } else
+                        Toast.makeText(this, "Wybierz co chciałbyś zwiedzić", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.bntAutomatic:
+                mAutomaticBnt.setEnabled(false);
+                mManualBnt.setEnabled(true);
+                setVisible();
+                mManualMode = false;
+                mOKbnt.setEnabled(true);
+                break;
+            case R.id.bntManual:
+                mAutomaticBnt.setEnabled(true);
+                mManualBnt.setEnabled(false);
+                setInvisible();
+                mManualMode = true;
+                mOKbnt.setEnabled(true);
+                break;
+        }
+        if (intent != null) {
+            startActivity(intent);
+        }
+    }
+
+    private void initUIreferences() {
         mDestinationTv = (AutoCompleteTextView) findViewById(R.id.etDestination);
         mOriginTv = (AutoCompleteTextView) findViewById(R.id.etOrigin);
 
@@ -71,75 +187,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mDurationTv = (TextView) findViewById(R.id.tvMinutes);
         mLengthSb = (SeekBar) findViewById(R.id.sbLength);
         mDurationSb = (SeekBar) findViewById(R.id.sbDuration);
-        mDurationSb.setMax(600 - 25); // minuty max 600 min 25
-        mDurationSb.setMin(0);
-        mLengthSb.setMax(500 - 20); //km  max 500 min 20
-        mLengthSb.setMin(0);
-        mLengthSb.setProgress(1);
-
-        // na poczatku sa ukryte
-        setInvisible();
-        mOKbnt.setEnabled(false);
-
-
-        mOKbnt.setOnClickListener(this);
-        mManualBnt.setOnClickListener(this);
-        mAutomaticBnt.setOnClickListener(this);
-        seekBarsChangeListener();
-
-        AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
-                .setCountry("PL")
-                .setTypeFilter(AutocompleteFilter.TYPE_FILTER_ADDRESS)
-                .build();
-
-        mGeoDataClient = Places.getGeoDataClient(this);
-        madapter = new PlaceAutocompleteArrayAdapter(this, mGeoDataClient, null, typeFilter);
-        mOriginTv.setAdapter(madapter);
-        mDestinationTv.setAdapter(madapter);
-
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        Intent intent = null;
-        switch (v.getId()) {
-            case R.id.bntOk:
-                if (checkEditTexts() && checkPlaceType()) {
-                    intent = new Intent(this, MapsActivity.class);
-                    intent.putExtra("DESTINATION", mDestinationTv.getText().toString());
-                    intent.putExtra("ORIGIN", mOriginTv.getText().toString());
-                    intent.putExtra("TRAVEL_MODE", checkTravelMode());
-
-                    if (mMuseumCb.isChecked())
-                        intent.putExtra("TYPE", "museum");
-                    else
-                        intent.putExtra("TYPE", "park");
-                } else {
-                    Toast.makeText(this, "Wprowadz lokalizacje", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            case R.id.bntAutomatic:
-                mAutomaticBnt.setEnabled(false);
-                mManualBnt.setEnabled(true);
-                setVisible();
-                break;
-            case R.id.bntManual:
-                mAutomaticBnt.setEnabled(true);
-                mManualBnt.setEnabled(false);
-                setInvisible();
-                break;
-            case R.id.rbCar:
-                //srednia predkosc ok 50km/h = 0.8333333333333 km/min
-                mDurationSb.setMax(600); // min
-                mDurationSb.setMin(25);
-                mLengthSb.setMax(500); //km
-                mLengthSb.setMin(20);
-
-        }
-        if (intent != null) {
-            startActivity(intent);
-        }
     }
 
     private boolean checkEditTexts() {
@@ -188,50 +235,103 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mLengthSb.setVisibility(View.GONE);
         mDurationSb.setVisibility(View.GONE);
     }
-    private void seekBarsChangeListener () {
+
+    private void seekBarsChangeListener() {
         mLengthSb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                mDistanceTv.setText((progress + 20) + "km");
-                if (fromUser) {
-                    mDistanceTv.setText((progress + 20) + "km");
-                    int pr = (int) (progress / 0.8333);
-                    mDurationSb.setProgress(pr);
+                switch (checkTravelMode()) {
+                    case "driving":
+                        mDistanceTv.setText((progress + 20) + " km");
+                        if (fromUser) {
+                            mDistanceTv.setText((progress + 20) + " km");
+                            int dur = (int) (progress / 0.8333);
+                            mDurationSb.setProgress(dur);
+                        }
+                        mDistance = progress + 20;
+                        Log.d(TAG, "onProgressChanged: distance " + mDistance);
+                        break;
+                    case "bicycling":
+                        mDistanceTv.setText((progress + 15) + " km");
+                        if (fromUser) {
+                            mDistanceTv.setText((progress + 15) + " km");
+                            int dur = (int) (progress / 0.2833);
+                            mDurationSb.setProgress(dur);
+                        }
+                        mDistance = progress + 15;
+                        Log.d(TAG, "onProgressChanged: distance " + mDistance);
+                        break;
+                    case "walking":
+                        mDistanceTv.setText((progress + 7) + " km");
+                        if (fromUser) {
+                            mDistanceTv.setText((progress + 7) + " km");
+                            int dur = (int) (progress / 0.075);
+                            mDurationSb.setProgress(dur);
+                        }
+                        mDistance = progress + 7;
+                        Log.d(TAG, "onProgressChanged: distance " + mDistance);
+                        break;
                 }
-
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
         mDurationSb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                mDurationTv.setText((progress + 25) + "min");
-                if (fromUser) {
-                    mDurationTv.setText((progress + 25) + "min");
-                    int pr = (int) (progress * 0.8333);
-                    mLengthSb.setProgress(pr);
+                switch (checkTravelMode()) {
+                    case "driving":
+                        mDurationTv.setText((convertTime(progress + 25)));
+                        if (fromUser) {
+                            mDurationTv.setText(convertTime(progress + 25));
+                            int dis = (int) (progress * 0.8333);
+                            mLengthSb.setProgress(dis);
+                        }
+                        mDuration = progress + 25;
+                        Log.d(TAG, "onProgressChanged: duration " + mDuration);
+                        break;
+                    case "bicycling":
+                        mDurationTv.setText(convertTime((progress + 53)));
+                        if (fromUser) {
+                            mDurationTv.setText(convertTime((progress + 53)));
+                            int dis = (int) (progress * 0.2833);
+                            mLengthSb.setProgress(dis);
+                        }
+                        mDuration = progress + 53;
+                        Log.d(TAG, "onProgressChanged: duration " + mDuration);
+                        break;
+                    case "walking":
+                        mDurationTv.setText(convertTime((progress + 93)));
+                        if (fromUser) {
+                            mDurationTv.setText(convertTime((progress + 93)));
+                            int dis = (int) (progress * 0.075);
+                            mLengthSb.setProgress(dis);
+                        }
+                        mDuration = progress + 93;
+                        Log.d(TAG, "onProgressChanged: duration " + mDuration);
+                        break;
                 }
-
             }
+
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
+
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
     }
 
-
+    private String convertTime(int minutes) {
+        return LocalTime.MIN.plus(
+                Duration.ofMinutes(minutes)
+        ).toString();
+    }
 }
